4cd297483a9fa593dee4622a296775692724b988
Trang Vu Phuong <leokage2006@gmail.com>
2026-05-28 11:36:11 +0700

    fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build 
 WORKDIR /app 
 COPY . . 
-RUN dotnet publish -c Release -o out 
+RUN dotnet publish Birthday2.csproj -c Release -o out 
+ 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 
 WORKDIR /app 
 COPY --from=build /app/out . 
--- obj/Debug/net8.0/Birthday2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Birthday2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Birthday2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62681847682620f0cc7b90e7d95af7c9d26f929b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db008e18214e4e77fb6ee156028abce280991bdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Birthday2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Birthday2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
