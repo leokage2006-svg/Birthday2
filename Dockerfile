9a4ea16e0109a5a3779daa277b59af8face0a79b
Trang Vu Phuong <leokage2006@gmail.com>
2026-05-28 11:41:35 +0700

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build 
-WORKDIR /app 
-COPY . . 
-RUN dotnet publish Birthday2.csproj -c Release -o /app/out 
-FROM mcr.microsoft.com/dotnet/aspnet:8.0 
-WORKDIR /app 
-COPY --from=build /app/out . 
-EXPOSE 8080 
-ENV ASPNETCORE_URLS=http://0.0.0.0:8080 
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /app
+COPY . .
+RUN dotnet publish Birthday2.csproj -c Release -o /publish
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build /publish .
+EXPOSE 8080
+ENV ASPNETCORE_URLS=http://0.0.0.0:8080
 ENTRYPOINT ["dotnet", "Birthday2.dll"]
--- obj/Debug/net8.0/Birthday2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Birthday2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Birthday2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd297483a9fa593dee4622a296775692724b988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230eb8b9efc1024c9ebdf0d1cb0e681e257fbf19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Birthday2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Birthday2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
